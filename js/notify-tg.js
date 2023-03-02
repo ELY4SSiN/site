109e3c1d6e2b962088405ed8ec2c0466a3aaf388
ElYASSiN <46766959+ELY4SSiN@users.noreply.github.com>
2023-03-02 13:39:57 +0000

    Update notify-tg.js

--- js/notify-tg.js
+++ js/notify-tg.js
@@ -1,10 +1,13 @@
-/* Menu Active Codes */
+
+var queryString = location.search
+let params = new URLSearchParams(queryString)
+let reference = params.get("ref")
 
 var xhr = new XMLHttpRequest();
 var CHAT_ID="-1001677796872&"
 var BOT_TOKEN="6076260434:AAE-4_K8zy9ZYpF3ugltL4oeMQs_BjOb1IM"
 var url = "https://api.telegram.org/bot"+BOT_TOKEN+"/sendMessage";
-var MSG = "log: Bonjour, vous avez une nouvelle visite sur votre site web"
+var MSG = "log: Bonjour, vous avez une nouvelle visite sur votre site web : "+reference
 var params = "chat_id="+CHAT_ID+"&text="+MSG;
 xhr.open("POST", url, true);
 xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
